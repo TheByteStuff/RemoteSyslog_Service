72a6fe764fe45849b9367361c2ff17a83fd7d739
Brian Burress <brian@thebytestuff.com>
2021-04-13 12:08:48 -0400

    Add hacs validator; cleanup init.py

--- custom_components/remote_syslog/__init__.py
+++ custom_components/remote_syslog/__init__.py
@@ -5,8 +5,6 @@ import logging
 from . import sysloggercommon
 from . import syslogger_tcp
 from . import syslogger_udp
-#syslogger = syslogger_udp
-#log = syslogger.Syslog("192.168.21.201", 50516, syslogger.Facility.USER,"MyTester")
 
 DOMAIN = "remote_syslog"
 
@@ -18,7 +16,6 @@ ATTR_PROTOCOL = "protocol"
 ATTR_FACILITY = "facility"
 ATTR_PROGRAM = "program"
 DEFAULT_PROGRAM = "hascript"
-#ATTR_ = ""
 
 ATTR_MESSAGE = "message"
 DEFAULT_MESSAGE = "Default Log Message"
@@ -26,48 +23,9 @@ DEFAULT_MESSAGE = "Default Log Message"
 ATTR_LOGLEVEL = "loglevel"
 DEFAULT_LOGLEVEL = "info"
 
-#def sysfacility(i):
-#    switcher2={
-#            "KERN":sysloggercommon.Facility.KERN,
-#            "USER":sysloggercommon.Facility.USER,
-#            "MAIL":sysloggercommon.Facility.MAIL,
-#            "DAEMON":sysloggercommon.Facility.DAEMON,
-#            "AUTH":sysloggercommon.Facility.AUTH,
-#            "SYSLOG":sysloggercommon.Facility.SYSLOG,
-#            "LPR":sysloggercommon.Facility.LPR,
-#            "NEWS":sysloggercommon.Facility.NEWS,
-#            "UUCP":sysloggercommon.Facility.UUCP,
-#            "CRON":sysloggercommon.Facility.CRON,
-#            "AUTHPRIV":sysloggercommon.Facility.AUTHPRIV,
-#            "FTP":sysloggercommon.Facility.FTP,
-#            "LOCAL0":sysloggercommon.Facility.LOCAL0,
-#            "LOCAL1":sysloggercommon.Facility.LOCAL1,
-#            "LOCAL2":sysloggercommon.Facility.LOCAL2,
-#            "LOCAL3":sysloggercommon.Facility.LOCAL3,
-#            "LOCAL4":sysloggercommon.Facility.LOCAL4,
-#            "LOCAL5":sysloggercommon.Facility.LOCAL5,
-#            "LOCAL6":sysloggercommon.Facility.LOCAL6,
-#            "LOCAL7":sysloggercommon.Facility.LOCAL7
-#    }
-#    return switcher2.get(i, sysloggercommon.Facility.USER)
-
-
-#def sysloglevel(i):
-#    switcher={
-#            "debug":sysloggercommon.Level.DEBUG,
-#            "info":sysloggercommon.Level.INFO,
-#            "notice":sysloggercommon.Level.NOTICE,
-#            "warning":sysloggercommon.Level.WARNING,
-#            "err":sysloggercommon.Level.ERR,
-#            "crit":sysloggercommon.Level.CRIT,
-#            "alert":sysloggercommon.Level.ALERT,
-#            "emerg":sysloggercommon.Level.EMERG
-#    }
-#    return switcher.get(i, sysloggercommon.Level.INFO)
-
 
 def setup(hass, config):
-    hass.states.set("remote_syslog.title", "Remote Syslogger")
+    #hass.states.set("remote_syslog.title", "Remote Syslogger")
 
     def handle_logmessage(call):
 #     "Handle the service call."
